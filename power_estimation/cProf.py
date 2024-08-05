f784343ab7bb6aa2453aaede99d3a462b8d8830b
Shaan Yadav <39740442+Shaan106@users.noreply.github.com>
2024-08-05 17:54:07 +0000

    high level power estimation

---
+++ power_estimation/cProf.py
@@ -0,0 +1,25 @@
+import cProfile
+import pstats
+import io
+import numpy as np
+from scipy import signal
+
+def your_compute_function(x, y):
+    z = np.add(x, y)
+    return signal.convolve(z, np.ones(10))
+
+# Prepare some sample data
+x = np.random.rand(1000)
+y = np.random.rand(1000)
+
+# Profile the function
+pr = cProfile.Profile()
+pr.enable()
+result = your_compute_function(x, y)
+pr.disable()
+
+# Print the results
+s = io.StringIO()
+ps = pstats.Stats(pr, stream=s).sort_stats('cumulative')
+ps.print_stats()
+print(s.getvalue())
