44e3f557c1fd899cad45fc20430194cf73d7882c
Bernardo Trevisan <bernardo.trevisan@users.noreply.github.com>
2024-10-10 04:12:55 +0000

    Automated autoyapf fixes

--- storage/storage_model.py
+++ storage/storage_model.py
@@ -25,37 +25,50 @@ from storage.utils import CellType, OpTarget
 # model.display_summary()
 # model.cleanup()
 
+
 class StorageModel:
     """
     A class to model storage performance from various workload and cell configurations.
     """
 
-    def __init__(self,
-                 read_frequency: int,
-                 write_frequency: int,
-                 read_size: int,
-                 write_size: int,
-                 cell_type: CellType,
-                 word_width: int = 16,
-                 process_node: int = 22, #TODO: figure out what this does / why it doesn't trigger new output file
-                 opt_target: OpTarget = OpTarget.ReadLatency,
-                 capacity: int = 1,
-                 bits_per_cell: int = 1):
-        self.nvm_explorer_path = Path(__file__).resolve().parent / "nvmexplorer"
+    def __init__(
+            self,
+            read_frequency: int,
+            write_frequency: int,
+            read_size: int,
+            write_size: int,
+            cell_type: CellType,
+            word_width: int = 16,
+            process_node:
+        int = 22,  #TODO: figure out what this does / why it doesn't trigger new output file
+            opt_target: OpTarget = OpTarget.ReadLatency,
+            capacity: int = 1,
+            bits_per_cell: int = 1):
+        self.nvm_explorer_path = Path(
+            __file__).resolve().parent / "nvmexplorer"
         self.config = {
             "experiment": {
-                "read_frequency": read_frequency,
-                "write_frequency": write_frequency,
-                "read_size": read_size,
-                "write_size": write_size,
+                "read_frequency":
+                read_frequency,
+                "write_frequency":
+                write_frequency,
+                "read_size":
+                read_size,
+                "write_size":
+                write_size,
                 "cell_type": [cell_type.value],
-                "process_node": process_node,
+                "process_node":
+                process_node,
                 "opt_target": [opt_target.value],
-                "word_width": word_width,
+                "word_width":
+                word_width,
                 "capacity": [capacity],
                 "bits_per_cell": [bits_per_cell],
-                "nvsim_path": str(self.nvm_explorer_path / "nvmexplorer_src" / "nvsim_src" / "nvsim"),
-                "output_path": str(self.nvm_explorer_path / "output"),
+                "nvsim_path":
+                str(self.nvm_explorer_path / "nvmexplorer_src" / "nvsim_src" /
+                    "nvsim"),
+                "output_path":
+                str(self.nvm_explorer_path / "output"),
             }
         }
         self.worst_case = None
@@ -67,10 +80,12 @@ class StorageModel:
         '''Runs model in nvmexplorer.'''
         print("Running model...")
         # check if nvsim executable exists
-        nvsim_path =  self.nvm_explorer_path / "nvmexplorer_src" / "nvsim_src" / "nvsim"
+        nvsim_path = self.nvm_explorer_path / "nvmexplorer_src" / "nvsim_src" / "nvsim"
         if not nvsim_path.exists():
-            raise FileNotFoundError(f"NVSim executable does not exist. Write 'make' in {nvsim_path.parent()}.")
-        
+            raise FileNotFoundError(
+                f"NVSim executable does not exist. Write 'make' in {nvsim_path.parent()}."
+            )
+
         # save config file in nvmexplorer
         self.save_config()
 
@@ -80,13 +95,17 @@ class StorageModel:
         try:
             result = subprocess.run(
                 [
-                    'python3', str(run_py_path),
-                    str(self.config_file_path.relative_to(self.nvm_explorer_path))
+                    'python3',
+                    str(run_py_path),
+                    str(
+                        self.config_file_path.relative_to(
+                            self.nvm_explorer_path))
                 ],
                 check=True,
                 capture_output=True,
                 text=True,
-                cwd=self.nvm_explorer_path  # Set the child directory as the working directory
+                cwd=self.
+                nvm_explorer_path  # Set the child directory as the working directory
             )
             print("Run Output:", result.stdout)
         except subprocess.CalledProcessError as e:
@@ -97,7 +116,7 @@ class StorageModel:
 
     def save_config(self):
         '''Saves config file in nvmexplorer's configs directory'''
-        
+
         with self.config_file_path.open('w') as file:
             json.dump(self.config, file, indent=4)
         print(f"Configuration saved to {self.config_file_path}")
