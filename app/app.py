489d7e68e29bd896bd24a52b543d9d2e0c4038aa
alegonshu <alegonshu@gmail.com>
2025-01-13 00:17:50 -0500

    integrate storage backend

--- app/app.py
+++ app/app.py
@@ -11,6 +11,9 @@ from asa.utils import INPUT_PE, generate_signal
 from asa import FFT, SVM, THR
 import numpy as np
 import csv
+from storage import StorageModel, CellType, ResultType
+import math
+
 
 app = Flask(__name__)
 app.config['STATIC_FOLDER'] = 'static'
@@ -303,6 +306,23 @@ def run_pipeline():
             "Power": power
         }
         output_data.append(element_output)
+    
+    model = StorageModel(
+        cell_type=CellType.STT, 
+        write_size=32 * math.prod(elements[-1].simulation_data['output_dimensions']),
+        time_constraint=input_samples / (input_fs * input_channels)
+    )
+    model.run()
+
+    storage_fields = [ResultType.TOTAL_POWER, ResultType.TOTAL_DYNAMIC_READ_POWER,
+                       ResultType.TOTAL_DYNAMIC_WRITE_POWER, ResultType.TOTAL_ENERGY,
+                       ResultType.TOTAL_LATENCY, ResultType.TOTAL_READ_LATENCY,
+                       ResultType.TOTAL_WRITE_LATENCY, ResultType.AREA, ResultType.LIFE_EXPECTANCY]
+    
+    storage_data = {}
+    for field in storage_fields:
+        storage_data[field.value] = model.get_result(field)
+    print(storage_data)
 
     # Save output
     filename = os.path.join(BASE_DIR, "pipelines", pipeline_data['name'], "d3",
--- storage/__init__.py
+++ storage/__init__.py
@@ -1,2 +1,4 @@
-from . import model
-from . import utils
+from .model import StorageModel
+from .utils import CellType, OpTarget, ResultType
+# from .flash import Flash, OpType, Mode
+# from .graphs import mem_type_comparison, channel_freq_plot
