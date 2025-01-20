58ff1b780af98cb6505d23e3a44a17b476e3353a
Bernardo Trevisan <bernardo.trevisan@users.noreply.github.com>
2025-01-20 07:27:32 +0000

    Automated autoyapf fixes

--- development-modules/datasets/SWEC_ETHZ_iEEG/scripts/torch_train_svm.py
+++ development-modules/datasets/SWEC_ETHZ_iEEG/scripts/torch_train_svm.py
@@ -131,9 +131,10 @@ y = torch.load('./model_data/in_label_tensor.pt')
 y = y * 2 - 1
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.20, random_state=42
-)
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.20,
+                                                    random_state=42)
 
 # # Define the SVM model
 # class SVM(nn.Module):
