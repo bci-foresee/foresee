93385b091cff2d2ef5284476466b6f583118a61d
Shaan Yadav <39740442+Shaan106@users.noreply.github.com>
2025-01-21 22:31:43 +0000

    shiao pipe power est

--- pipelines/sandbox/test_shiao_full.py
+++ pipelines/sandbox/test_shiao_full.py
@@ -36,8 +36,8 @@ def test_sandbox_pipeline() -> None:
                  berger_bands=[(0.1, 4), (4, 8), (8, 12), (12, 30), (30, 80),
                                (80, 180)],
                  clk=1,
-                 rtl_sim=True,
-                 rtl_power_estimation=True,
+                 rtl_sim=False,
+                 rtl_power_estimation=False,
                  save_visualization=False)
 
     # pwxc
